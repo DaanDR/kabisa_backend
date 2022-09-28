1e8b2a8a8c2cc4b1b4968f0b68a4bad36a166ed2
DBXDRD <daan.de.rond@kvk.nl>
2022-09-28 15:15:19 +0200

    #boyscout: no * import

--- src/main/java/com/example/kabisa_backend/validation/ValidId.java
+++ src/main/java/com/example/kabisa_backend/validation/ValidId.java
@@ -3,7 +3,11 @@ package com.example.kabisa_backend.validation;
 import javax.validation.Constraint;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Documented
 @Constraint(validatedBy = ValidId.IdValidator.class)
